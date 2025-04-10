ccf31ac654dc70caeed261777c51242535f762ab
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2025-04-10 07:35:55 +0530

    mlua template

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use mlua::prelude::*;
+
+fn add(_lua: &Lua, (a, b): (mlua::Integer, mlua::Integer)) -> LuaResult<mlua::Integer> {
+    Ok(a + b)
+}
+
+#[mlua::lua_module]
+fn addition(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("add", lua.create_function(add)?)?;
+    Ok(exports)
+}
